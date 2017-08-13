88dbd2c597d826d56422043f98c034584214d089
Ryan Tsang <ryantsangai@gmail.com>
2017-08-13 11:33:03 +0800

    merge PR https://github.com/paulyoung/fontello-cli/pull/31/files

--- src/fontello.js
+++ src/fontello.js
@@ -8,13 +8,8 @@ const unzip = require('unzip');
 
 const HOST = 'http://fontello.com';
 
-
-const apiRequest = function(options, successCallback, errorCallback) {
-  if (options.host == null) { options.host = HOST; }
-
-  const requestOptions = { multipart: true };
-  if (options.proxy != null) { requestOptions.proxy = options.proxy; }
-
+const getSession = function(options, requestOptions, successCallback, errorCallback) {
+  print('Creating a new session'.green);
   const data = {
     config: {
       file: options.config,
@@ -27,6 +22,13 @@ const apiRequest = function(options, successCallback, errorCallback) {
     const sessionId = body;
 
     if (response.statusCode === 200) {
+      fs.writeFile('.fontello-session', sessionId, function(err) {
+          if (!err) {
+            return print('Session was saved as .fontello-session \n'.green);
+          } else {
+            return print(err + "\n");
+          }
+      });
       const sessionUrl = `${options.host}/${sessionId}`;
       return (typeof successCallback === 'function' ? successCallback(sessionUrl) : undefined);
     } else {
@@ -34,6 +36,29 @@ const apiRequest = function(options, successCallback, errorCallback) {
     }
   });
 };
+
+const apiRequest = function(options, successCallback, errorCallback) {
+  if (options.host == null) { options.host = HOST; }
+
+  const requestOptions = { multipart: true };
+  if (options.proxy != null) { requestOptions.proxy = options.proxy; }
+  if (fs.existsSync(".fontello-session")) {
+    const stats = fs.statSync(".fontello-session");
+
+    const timeDiff = Math.abs(new Date().getTime() - stats.mtime.getTime());
+
+    if (timeDiff < (1000 * 3600 * 24)) {
+      print('Using .fontello-session'.green);
+      const sessionId = fs.readFileSync('.fontello-session');
+      const sessionUrl = `${options.host}/${sessionId}`;
+      return (typeof successCallback === 'function' ? successCallback(sessionUrl) : undefined);
+    }
+  }
+
+
+  return getSession(options, requestOptions, successCallback, errorCallback);
+};
+
 
 
 const fontello = {
@@ -43,7 +68,6 @@ const fontello = {
     // Begin the download
     //
     return apiRequest(options, function(sessionUrl) {
-
       const requestOptions = {};
       if (options.proxy != null) { requestOptions.proxy = options.proxy; }
 
@@ -94,7 +118,6 @@ const fontello = {
 
 
 module.exports = fontello;
-
 function __guard__(value, transform) {
   return (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined;
 }
